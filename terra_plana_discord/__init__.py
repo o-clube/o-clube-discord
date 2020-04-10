8976470a442df2e0951d4bd61ce4e6352ce18446
Matheus C. Zaglia <mzaglia@gmail.com>
2020-04-10 20:30:23 -0300

    Add rito command to get LoL active match info

--- terra_plana_discord/__init__.py
+++ terra_plana_discord/__init__.py
@@ -2,7 +2,9 @@ import discord
 import json
 import markovify
 import praw
+import requests
 from discord.ext import commands
+from beautifultable import BeautifulTable
 
 import os
 
@@ -39,6 +41,56 @@ async def bozo(ctx):
     '''Generate a 280 char text from Bolsonaro\'s tweets.'''
     await ctx.send(model.make_short_sentence(280))
 
+@bot.command()
+async def rito(ctx, *, summoner):
+    '''Searchs for a live match in the RIOT API.'''
+    riot_url = 'https://br1.api.riotgames.com'
+    summoner_api = f'/lol/summoner/v4/summoners/by-name/{summoner}'
+    headers={'X-Riot-Token':os.getenv('RIOT_TOKEN')}
+
+    summoner_resp = requests.get(riot_url+summoner_api, headers=headers)
+
+    if summoner_resp.status_code == 404:
+        await ctx.send('''```
+                       Invocador não encontrado!```''')
+
+    summoner_resp = summoner_resp.json()
+
+    account_id = summoner_resp['id']
+    spec_api=f'/lol/spectator/v4/active-games/by-summoner/{account_id}'
+
+    spec_resp = requests.get(riot_url+spec_api, headers=headers)
+
+    if spec_resp.status_code == 404:
+        await ctx.send('''```
+                       Partida não encontrada!```''')
+
+    players = spec_resp.json()['participants']
+    elos = list()
+
+    for player in players:
+        ranked_stats = f'/lol/league/v4/entries/by-summoner/{player["summonerId"]}'
+        resp = requests.get(riot_url+ranked_stats, headers=headers).json()
+        has_elo = False
+
+        for i in resp:
+            if i['queueType'] == 'RANKED_SOLO_5x5':
+                has_elo = True
+                elos.append(f'{i["tier"]} {i["rank"]}')
+                break
+
+        if not has_elo:
+            elos.append('UNRANKED')
+
+    t = BeautifulTable()
+    t.set_style(BeautifulTable.STYLE_NONE)
+    t.append_row([elos[0], players[0]['summonerName'], 'x', players[5]['summonerName'], elos[5]])
+    t.append_row([elos[1], players[1]['summonerName'], 'x', players[6]['summonerName'], elos[6]])
+    t.append_row([elos[2], players[2]['summonerName'], 'x', players[7]['summonerName'], elos[7]])
+    t.append_row([elos[3], players[3]['summonerName'], 'x', players[8]['summonerName'], elos[8]])
+    t.append_row([elos[4], players[4]['summonerName'], 'x', players[9]['summonerName'], elos[9]])
+
+    await ctx.send(f'''```\n{t}```''')
 
 bot.run(os.getenv('DISCORD_TOKEN'))
 

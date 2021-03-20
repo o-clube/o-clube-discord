df0cdb9528f18c626a30473862f095bbb67fef09
Matheus C. Zaglia <mzaglia@gmail.com>
2021-03-19 23:52:26 -0300

    :loud_sound: Enable logs

--- bot.py
+++ bot.py
@@ -1,18 +1,21 @@
 # https://discord.com/api/oauth2/authorize?client_id=817794806738059334&permissions=2416143440&scope=bot
+import logging
 import traceback
+
 from os import listdir
 from os.path import isfile, join
 
 from discord import Intents
 from discord.ext import commands
 
+logging.basicConfig(level=logging.INFO)
 
 def load_extensions(bot: commands.Bot, cogs_dir: str = "cogs"):
     for extension in [f.replace(".py", "") for f in listdir(cogs_dir) if isfile(join(cogs_dir, f))]:
         try:
             bot.load_extension(cogs_dir + "." + extension)
         except Exception as e:
-            print(f"Failed to load extension {extension}.")
+            logging.error(f"Failed to load extension {extension}.")
             traceback.print_exc()
 
 

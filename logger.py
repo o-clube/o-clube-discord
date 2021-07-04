bee2c42deb95817e9121266f6663ede54ea131ef
Matheus C. Zaglia <mzaglia@gmail.com>
2021-07-04 20:15:25 -0300

    :recycle: Adding a limiter to DiscordHandler due to rate limiting

--- logger.py
+++ logger.py
@@ -5,6 +5,7 @@ import asyncio
 import logging
 from logging import StreamHandler
 
+from aiolimiter import AsyncLimiter
 from discord import AsyncWebhookAdapter, Color, Embed, Color, Webhook
 import discord
 
@@ -20,29 +21,31 @@ class DiscordHandler(StreamHandler):
             "DEBUG": Color.dark_green(),
             "NOTSET": Color.dark_grey(),
         }
+        self.limiter = AsyncLimiter(1, 1)
         super(DiscordHandler, self).__init__()
 
     def emit(self, record):
         asyncio.create_task(self.emitting(record))
 
     async def emitting(self, record):
+        async with self.limiter:
+            async with aiohttp.ClientSession() as session:
+                webhook = Webhook.from_url(self.webhook, adapter=AsyncWebhookAdapter(session))
 
-        async with aiohttp.ClientSession() as session:
-            webhook = Webhook.from_url(self.webhook, adapter=AsyncWebhookAdapter(session))
+                record.pathname = record.pathname.replace(os.getcwd(), "")[1:]
 
-            record.pathname = record.pathname.replace(os.getcwd(), "")[1:]
+                description = self.format(record)
 
-            description = self.format(record)
+                for y in range(2000, len(description) + 2000, 2000):
+                    try:
+                        embed = Embed(color=self.level_color[record.levelname], description=description[y - 2000 : y])
 
-            for y in range(2000, len(description) + 2000, 2000):
-                try:
-                    embed = Embed(color=self.level_color[record.levelname], description=description[y - 2000 : y])
+                        await webhook.send(
+                            embed=embed,
+                            username="O Clube",
+                            avatar_url="https://cdn.discordapp.com/avatars/326107366275940362/1cc62dd7b583a7d7fa6b7698fd280404",
+                        )
 
-                    await webhook.send(
-                        embed=embed,
-                        username="O Clube",
-                        avatar_url="https://cdn.discordapp.com/avatars/326107366275940362/1cc62dd7b583a7d7fa6b7698fd280404",
-                    )
-                except discord.errors.HTTPException as e:
-                    logger = logging.getLogger('discord')
-                    logger.critical("Error sending log to discord webhook.", exc_info=e)
+                    except discord.errors.HTTPException as e:
+                        logger = logging.getLogger('discord')
+                        logger.critical("Error sending log to discord webhook.", exc_info=e)

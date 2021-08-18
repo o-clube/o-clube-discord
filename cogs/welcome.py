0f2c2169cdd5e976f1b7e14b62561d2704a32013
Felipe Ferraz <felipe.martins@solinftec.com>
2021-08-18 17:11:11 -0300

    :art: Improve welcome

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 
 
-from date_utils import DayPeriod, get_day_period
+from date_utils import DayPeriod, get_day_period, check_day_period
 from models import session, User
 
 
@@ -26,29 +26,17 @@ class Welcome(Cog):
         """Plays a welcome sound message when someone joins the channel."""
         if before.channel == None and after.channel != None and not member.bot and not after.self_deaf:
             now = arrow.now('America/Sao_Paulo')
-            userList = []
-            for onlineMember in after.channel.members:
-                user = session.query(User).filter_by(member_id=onlineMember.id).first()
-                if not user:
-                    user = User(
-                        member_id=onlineMember.id,
-                        guild_id=after.channel.guild.id,
-                        name=onlineMember.name,
-                        last_seen=now
-                        )
-                    session.add(user)
-                    userList.append(user)
-                    continue
-                
-                if (now - user.last_seen).total_seconds() < 3600 * 12:
-                    continue
-                elif not onlineMember.voice.self_deaf:
-                    userList.append(user)
-                
-
-            if userList:
+            user = session.query(User).filter_by(member_id=member.id).first()
+            if not user:
+                user = User(
+                    member_id=member.id,
+                    guild_id=after.channel.guild.id,
+                    name=member.name,
+                    last_seen=now.shift(hours=-24) # To have the bot welcome a new user who has joined the server for the first time
+                    )
+                session.add(user)
+            if(check_day_period(user.last_seen) != check_day_period(now) or (now - user.last_seen).total_seconds() > 3600 * 12):
                 day_period = get_day_period()
-                f = None
                 if day_period == DayPeriod.MORNING:
                     f = 'data/welcome/mourao.mp3'
                 elif now.hour >= 12 and now.hour < 13:
@@ -65,9 +53,53 @@ class Welcome(Cog):
                 while vc.is_playing():
                     await asyncio.sleep(.5)
                 await vc.disconnect()
-                for user in userList:
-                    user.last_seen = now
+                user.last_seen = now
                 session.commit()
+
+
+            
+            # userList = []
+            # for onlineMember in after.channel.members:
+            #     user = session.query(User).filter_by(member_id=onlineMember.id).first()
+            #     if not user:
+            #         user = User(
+            #             member_id=onlineMember.id,
+            #             guild_id=after.channel.guild.id,
+            #             name=onlineMember.name,
+            #             last_seen=now
+            #             )
+            #         session.add(user)
+            #         userList.append(user)
+            #         continue
+                
+            #     if (now - user.last_seen).total_seconds() < 3600 * 12:
+            #         continue
+            #     elif not onlineMember.voice.self_deaf:
+            #         userList.append(user)
+                
+
+            # if userList:
+            #     day_period = get_day_period()
+            #     f = None
+            #     if day_period == DayPeriod.MORNING:
+            #         f = 'data/welcome/mourao.mp3'
+            #     elif now.hour >= 12 and now.hour < 13:
+            #         f = 'data/welcome/dilma.mp3'
+            #     elif day_period == DayPeriod.AFTERNOON:
+            #         f = 'data/welcome/jornalhoje.mp3'
+            #     else:
+            #         f = 'data/welcome/bonner.mp3'
+
+            #     audio = FFmpegPCMAudio(f)
+            #     vc = await after.channel.connect()
+            #     vc.play(audio)
+
+            #     while vc.is_playing():
+            #         await asyncio.sleep(.5)
+            #     await vc.disconnect()
+            #     for user in userList:
+            #         user.last_seen = now
+            #     session.commit()
 
 
 
